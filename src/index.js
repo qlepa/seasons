cbb6c86a81bb50f1b8b14ca5d961143c061c1778
Tomasz Klepacki <t.klepacki@gmail.com>
2018-11-19 17:42:36 +0100

    Start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-import Eact from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
+
+    return (
+        <div>Latitude: </div>
+    );
+
+};
+
+ReactDOM.render(
+    <App/>, document.querySelector('#root')
+);
